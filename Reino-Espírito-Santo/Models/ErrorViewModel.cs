d3f927cb7803b1493c67c26020136a3985b9b286
Lucas Salvador <lucassalvador948@gmail.com>
2025-01-20 19:31:09 -0300

    Add files via upload

---
+++ Reino-Espírito-Santo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reino_Espírito_Santo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
