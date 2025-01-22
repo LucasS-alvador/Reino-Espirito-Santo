d7eb33b1d89f0893426efbf5dd82c643f42c1d1d
tyggas <tagrigull@gmail.com>
2025-01-22 20:22:32 -0300

    missionarios adicionados

---
+++ Reino-Espírito-Santo/Models/Missionarios/MissionarioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Reino_Espírito_Santo.Models.Missionarios
+{
+    public class MissionarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
