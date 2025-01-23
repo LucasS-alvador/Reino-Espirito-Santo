a3bddeca92b7a3acaaad104f4f656c4e212181c9
BRUNO DE MATOS <bruno.matos1@alunos.sc.senac.br>
2025-01-22 21:08:57 -0300

    Feature de Dizimos

---
+++ Reino-Espírito-Santo/Controllers/DizimosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reino_Espírito_Santo.Models.Dizimo;
+
+namespace Reino_Espírito_Santo.Controllers
+{
+    public class DizimosController : Controller
+    {
+        public static decimal _Total = 100;
+        public static decimal _UltimaContribuicao = 20;
+
+        public IActionResult Index()
+        {
+            var model = new DizimoModel () { Total = _Total, Adicionado = _UltimaContribuicao };
+            return View(model);
+        }
+        public IActionResult Contribuir()
+        {
+            return View(new DizimoModel () { Adicionado = 0});
+        }
+        [HttpPost]
+        public IActionResult Contribuir(DizimoModel model)
+        {
+            _UltimaContribuicao = model.Adicionado;
+            _Total += model.Adicionado;
+
+            var updatedModel = new DizimoModel() { Total = _Total, Adicionado = model.Adicionado };
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
---
+++ Reino-Espírito-Santo/Models/Dizimos/DizimoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Reino_Espírito_Santo.Models.Dizimo
+{
+    public class DizimoModel
+    {
+        public int Id { get; set; }
+        public decimal Total { get; set; }
+        public decimal Valor { get; set; }
+        public decimal Adicionado {  get; set; }
+
+    }
+}
