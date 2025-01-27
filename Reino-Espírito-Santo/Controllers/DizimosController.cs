7cc3b9493e3fd13d031d7db4474897e2d53b353b
BRUNO DE MATOS <bruno.matos1@alunos.sc.senac.br>
2025-01-27 20:25:23 -0300

    Adicionando uma lista aos adicionados

--- Reino-Espírito-Santo/Controllers/DizimosController.cs
+++ Reino-Espírito-Santo/Controllers/DizimosController.cs
@@ -5,28 +5,36 @@ namespace Reino_Espírito_Santo.Controllers
 {
     public class DizimosController : Controller
     {
-        public static decimal _Total = 100;
-        public static decimal _UltimaContribuicao = 20;
+        public static decimal _Total = 0;
+        public static decimal _UltimaContribuicao = 0;
+        // Lista de contribuições antigas
+        public static List<decimal> _AdicionadosAntigos = new List<decimal> {};
 
         public IActionResult Index()
         {
-            var model = new DizimoModel () { Total = _Total, Adicionado = _UltimaContribuicao };
+            var model = new DizimoModel()
+            {
+                Total = _Total,
+                Adicionado = _UltimaContribuicao,
+                AdicionadosAntigos = _AdicionadosAntigos // Passando a lista de antigos
+            };
             return View(model);
         }
+
         public IActionResult Contribuir()
         {
-            return View(new DizimoModel () { Adicionado = 0});
+            return View(new DizimoModel() { Adicionado = 0 });
         }
+
         [HttpPost]
         public IActionResult Contribuir(DizimoModel model)
         {
             _UltimaContribuicao = model.Adicionado;
             _Total += model.Adicionado;
 
-            var updatedModel = new DizimoModel() { Total = _Total, Adicionado = model.Adicionado };
+            _AdicionadosAntigos.Add(model.Adicionado); // Adicionando o novo valor à lista de antigos
+
             return RedirectToAction("Index");
-
-
         }
     }
 }
--- Reino-Espírito-Santo/Models/Dizimos/DizimoModel.cs
+++ Reino-Espírito-Santo/Models/Dizimos/DizimoModel.cs
@@ -4,8 +4,9 @@
     {
         public int Id { get; set; }
         public decimal Total { get; set; }
-        public decimal Valor { get; set; }
-        public decimal Adicionado {  get; set; }
+        public decimal Adicionado { get; set; }
 
+        // Lista de contribuições anteriores
+        public List<decimal> AdicionadosAntigos { get; set; } = new List<decimal>();
     }
 }
