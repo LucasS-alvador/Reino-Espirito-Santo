c8023cd15822708a70808044e9dc57343b458964
tyggas <tagrigull@gmail.com>
2025-01-30 21:51:58 -0300

    controller ajustado

--- Reino-Espírito-Santo/Controllers/MinisteriosController.cs
+++ Reino-Espírito-Santo/Controllers/MinisteriosController.cs
@@ -1,53 +1,66 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Reino_Espírito_Santo.Models.Auxiliares;
-using Reino_Espírito_Santo.Models.Ministerio;
+using Reino_Espírito_Santo.DataBase.Entidades;
 using Reino_Espírito_Santo.Models.Ministerios;
+using Reino_Espírito_Santo.DataBase.Entidades;
 
 namespace Reino_Espírito_Santo.Controllers
 {
     public class MinisteriosController : Controller
     {
-        static List<MinisterioModel> _Ministerio = new List<MinisterioModel>()
-        {
-            new MinisterioModel(){Id = 1, Nome = "ministerio1",  Descricao = "Ministério de Louvor", auxiliarId = 3, DataInicio = new DateTime(2020, 1, 1)},
-            new MinisterioModel(){Id = 2, Nome = "ministerio2",   Descricao = "Ministério de Ensino", auxiliarId = 3, DataInicio = new DateTime(2019, 5, 15)},
-            new MinisterioModel(){Id = 3, Nome = "ministerio3",  Descricao = "Ministério de Oração", auxiliarId = 3, DataInicio = new DateTime(2021, 8, 30)},
-        };
-
-
-
         public IActionResult Index()
         {
-<<<<<<< HEAD
-            var model = new MinisteriosModel()
-            {
-                // Para cada ministério, adicione o nome do auxiliar, se existir.
-                Ministerio = _Ministerio.Select(ministerio => new MinisterioModel
-                {
-                    Id = ministerio.Id,
-                    Nome = ministerio.Nome,
-                    Descricao = ministerio.Descricao,
-                    auxiliarId = ministerio.auxiliarId,
-                    DataInicio = ministerio.DataInicio,
-                    // Aqui você atribui o nome do auxiliar com base no auxiliarId
-                    AuxiliarNome = AuxiliaresController.auxiliares
-                        .FirstOrDefault(a => a.Id == ministerio.auxiliarId)?.Nome ?? "Sem líder"
-                }).ToList()
-            };
+            var model = new MinisteriosModel();
+            model.Ministerio = new List<MinisterioModel>();
 
-=======
-            var model = new MinisteriosModel() { Ministerio = MinisterioModel.GetAll() };
->>>>>>> e09b85956de8bf155ee0360ca447ef085c6a0683
+            var ministerios = Ministerio.GetAll(); // Método estático para buscar todos os ministérios
+
+            // A forma mais simples de mapear os dados
+            model.Ministerio = ministerios.Select(ministerioEntidade => new MinisterioModel(ministerioEntidade)).ToList();
+
             return View(model);
         }
 
-
-        public IActionResult Record(long id)
+        public IActionResult Record(long? id)
         {
-            var missionarioAtual = _Ministerio.FirstOrDefault(missionario => missionario.Id == id);
-            return View(missionarioAtual);
+            var model = new MinisterioModel();
+
+            if (id.HasValue)
+            {
+                // Aqui buscamos o ministério pelo ID
+                model = new MinisterioModel(Ministerio.Get(id.Value));
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Record(MinisterioModel model, string type)
+        {
+            Ministerio ministerio = model.getEntidade();
+
+            if (type == "save")
+            {
+                // Se o ID for maior que 0, significa que já existe no banco, então vamos atualizar
+                if (ministerio.Id > 0)
+                {
+                    ministerio.Update(); // Atualiza o ministério no banco de dados
+                }
+                else
+                {
+                    ministerio.Create(); // Cria um novo ministério no banco de dados
+                }
+            }
+            else if (type == "delete")
+            {
+                ministerio.Delete(); // Exclui o ministério
+            }
+            else
+            {
+                return BadRequest("Requisição inválida!"); // Retorna erro se o tipo de requisição for inválido
+            }
+
+            return RedirectToAction("Index"); // Redireciona para a lista de ministérios
         }
 
         public IActionResult Adicionar()
@@ -60,40 +73,24 @@ namespace Reino_Espírito_Santo.Controllers
                 })
                 .ToList();
 
-            ViewBag.Auxiliares = auxiliares;
+            ViewBag.Auxiliares = auxiliares; // Passa os auxiliares para a view
 
             return View();
         }
 
-
         [HttpPost]
-
         public IActionResult Adicionar(MinisterioModel model)
         {
             if (ModelState.IsValid)
             {
-                model.Id = _Ministerio.Max(a => a.Id) + 1;
+                var ministerio = model.getEntidade(); // Converte o modelo para a entidade do banco
 
-                _Ministerio.Add(model);
+                ministerio.Create(); // Cria o novo ministério no banco
 
-                return RedirectToAction("Index");
+                return RedirectToAction("Index"); // Redireciona para a lista de ministérios
             }
 
-            return View(model);
-        }
-        public IActionResult Salvar(MinisterioModel model)
-        {
-            var missionario = _Ministerio.FirstOrDefault(i => i.Id == model.Id);
-
-            if (missionario != null) // Verifica se o missionário foi encontrado
-            {
-                missionario.Nome = model.Nome;
-                missionario.Descricao = model.Descricao;
-                missionario.auxiliarId = model.auxiliarId;
-                missionario.DataInicio = model.DataInicio;
-            }
-
-            return RedirectToAction("Index");
+            return View(model); // Retorna para a view caso a validação não passe
         }
     }
 }
