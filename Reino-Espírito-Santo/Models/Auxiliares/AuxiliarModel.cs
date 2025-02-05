95a7afb57d218cdc76f69dd3e02c0c723accdac3
MIGUEL LONNI DO PRADO <miguel.prado@alunos.sc.senac.br>
2025-02-05 19:20:02 -0300

    EntidadeBase

--- Reino-Espírito-Santo/Models/Auxiliares/AuxiliarModel.cs
+++ Reino-Espírito-Santo/Models/Auxiliares/AuxiliarModel.cs
@@ -6,109 +6,36 @@ using Reino_Espírito_Santo.DataBase;
 
 namespace Reino_Espírito_Santo.Models.Auxiliares
 {
-    public class AuxiliarModel
+    public class AuxiliarModel : EntidadeBase<AuxiliarModel>
     {
-        public int Id { get; set; }
         public string Nome { get; set; }
         public string Funcao { get; set; }
         public string Telefone { get; set; }
-
-        public static AuxiliarModel Get(int id)
+        public override List<string> Fields => new List<string>()
         {
-            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
-            {
-                conn.Open();
-                var querry = $"SELECT * FROM AUXILIARES WHERE ID = {id}";
-                MySqlCommand cmd = new MySqlCommand(querry, conn);
-                var reader = cmd.ExecuteReader();
+            "ID",
+            "NOME",
+            "FUNCAO",
+            "TELEFONE",
+        };
+        public override string TableName => "AUXILIARES";
+        public override AuxiliarModel Fill(MySqlDataReader reader)
+        { 
+            var aux = new AuxiliarModel();
 
-                if (reader.Read())
-                {
-                    return new AuxiliarModel
-                    {
-                        Id = reader.GetInt32("ID"),
-                        Nome = reader.GetString("NOME"),
-                        Funcao = reader.GetString("FUNCAO"),
-                        Telefone = reader.GetString("TELEFONE"),
-                    };
-                }
-            }
-            return null;
+            aux.Id = reader.GetInt32("ID");
+            aux.Nome = reader.GetString("NOME");
+            aux.Funcao = reader.GetString("FUNCAO");
+            aux.Telefone = reader.GetString("TELEFONE");
+
+            return aux;
         }
-        public static List<AuxiliarModel> GetAll()
+
+        public override void FillParameters(MySqlParameterCollection parameters)
         {
-            var result = new List<AuxiliarModel>();
-
-            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
-            {
-                conn.Open();
-                var query = "SELECT * FROM AUXILIARES";
-                var cmd = new MySqlCommand(query, conn);
-
-                var reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    result.Add(new AuxiliarModel()
-                    {
-                        Id = reader.GetInt32("ID"),
-                        Nome = reader.GetString("NOME"),
-                        Funcao = reader.GetString("FUNCAO"),
-                        Telefone = reader.GetString("TELEFONE"),
-                    });
-                }
-            }
-
-            return result;
-        }
-        public static void Create(AuxiliarModel c)
-        {
-            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
-            {
-                conn.Open();
-
-                var cmd = conn.CreateCommand();
-                cmd.CommandText = @"INSERT INTO AUXILIARES (NOME, FUNCAO, TELEFONE) 
-                                    VALUES (@pNOME, @pFUNCAO, @pTELEFONE)";
-
-                cmd.Parameters.Add(new MySqlParameter("pNOME", c.Nome));
-                cmd.Parameters.Add(new MySqlParameter("pFUNCAO", c.Funcao));
-                cmd.Parameters.Add(new MySqlParameter("pTELEFONE", c.Telefone));
-
-                cmd.ExecuteNonQuery();
-            }
-        }
-        public void Update()
-        {
-            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
-            {
-                conn.Open();
-
-                var cmd = conn.CreateCommand();
-                cmd.CommandText = @$"UPDATE AUXILIARES SET NOME = @pNOME, FUNCAO = @pFUNCAO, TELEFONE = @pTELEFONE
-                                    WHERE ID = @pID";
-
-                cmd.Parameters.Add(new MySqlParameter("pID", Id));
-                cmd.Parameters.Add(new MySqlParameter("pNOME", Nome));
-                cmd.Parameters.Add(new MySqlParameter("pFUNCAO", Funcao));
-                cmd.Parameters.Add(new MySqlParameter("pTELEFONE", Telefone));
-
-                cmd.ExecuteNonQuery();
-
-            }
-
-        }
-        public static void Delete(int id)
-        {
-            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
-            {
-                conn.Open();
-
-                var cmd = conn.CreateCommand();
-                cmd.Parameters.Add(new MySqlParameter("pID", id));
-                cmd.CommandText = @$"DELETE FROM AUXILIARES WHERE ID = @pID";
-
-                cmd.ExecuteNonQuery();
-            }
+            parameters.Add(new MySqlParameter("pNome", Nome));
+            parameters.Add(new MySqlParameter("pFUNCAO", Funcao));
+            parameters.Add(new MySqlParameter("pTELEFONE", Telefone));
         }
     }
 }
---
+++ Reino-Espírito-Santo/Models/EntidadeBase.cs
@@ -0,0 +1,98 @@
+﻿using Reino_Espírito_Santo.DataBase;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Reino_Espírito_Santo.Models
+{
+    public abstract class EntidadeBase<T>
+    {
+        public long Id { get; set; }
+        public abstract string TableName { get; }
+        public abstract List<String> Fields { get;}
+        public abstract T Fill(MySqlDataReader reader);
+        public abstract void FillParameters(MySqlParameterCollection parameters);
+        public T Get(long id)
+        {
+            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
+            {
+                conn.Open();
+                var querry = $"SELECT * FROM {TableName} WHERE ID = @ID";
+                var command = new MySqlCommand(querry, conn);
+                command.Parameters.Add(new MySqlParameter("ID", id));
+
+                var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    return Fill(reader);
+                }
+
+            }
+            return default(T);
+        }
+
+
+        public List<T> GetAll()
+        {
+            var result = new List<T>();
+
+            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
+            {
+                conn.Open();
+                var querry = $"SELECT * FROM {TableName}";
+                var command = new MySqlCommand(querry, conn);
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    result.Add(Fill(reader));
+                }
+
+            }
+            return result;
+        }
+
+        // Add a object to the database with the caller's vallues
+        public void Create()
+        {
+            using (var conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = @$"INSERT INTO {TableName} ({string.Join(",", Fields)}) VALUES ({string.Join(", ", Fields.Select(campo => $"@p{campo}"))})";
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // Update a object in the database with the caller's vallues
+        public void Update(long id)
+        {
+            using (var conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = @$"UPDATE {TableName} SET {string.Join(", ", Fields.Select(campo => $"{campo} = @p{campo}"))}
+                                   WHERE ID = @pID";
+                cmd.Parameters.Add(new MySqlParameter("ID", id));
+                FillParameters(cmd.Parameters);
+
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+        public void Delete(long id)
+        {
+            using (MySqlConnection conn = new MySqlConnection(DBConnection.CONNECTION_STRING))
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+                cmd.Parameters.Add(new MySqlParameter("pID", id));
+                cmd.CommandText = @$"DELETE FROM {TableName} WHERE ID = @pID";
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
