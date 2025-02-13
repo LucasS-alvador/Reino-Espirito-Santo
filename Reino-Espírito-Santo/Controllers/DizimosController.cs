f7f648230251aa1011cfb79319764030a7e31720
BRUNO DE MATOS <bruno.matos1@alunos.sc.senac.br>
2025-02-12 21:44:12 -0300

    Requisições

--- Reino-Espírito-Santo/Controllers/DizimosController.cs
+++ Reino-Espírito-Santo/Controllers/DizimosController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySqlX.XDevAPI.Common;
+using Reino_Espírito_Santo.Models.Auxiliares;
 using Reino_Espírito_Santo.Models.Dizimo;
 using System.Linq;
 
@@ -60,6 +62,55 @@ namespace Reino_Espírito_Santo.Controllers
             novoDizimo.Create();
 
             return RedirectToAction("Index");
+        }
+        [HttpGet("api/v1/Dizimos")]
+        public IActionResult Get()
+        {
+
+            var contribuicoes = _dizimoModel.GetAll();
+            var ultimoDizimo = contribuicoes.FirstOrDefault();
+
+            if (ultimoDizimo == null)
+            {
+                return NotFound("Nada encontrado");
+            }
+
+            var result = new 
+            { 
+                Total = ultimoDizimo.Total,
+                Adicionado = ultimoDizimo.Adicionado,
+                AntigoAdd1 = ultimoDizimo.AntigoAdd1,
+                AntigoAdd2 = ultimoDizimo.AntigoAdd2,
+                AntigoAdd3 = ultimoDizimo.AntigoAdd3
+            };
+
+
+            return Ok(result);
+        }
+        [HttpPost("api/v1/Dizimos")]
+        public IActionResult Post([FromBody] DizimoModel dizimo)
+        {
+            dizimo.Create();
+
+            return Ok("Carro Cadastrado");
+        }
+
+        [HttpPut("api/v1/Dizimos/{id}")]
+        public IActionResult Put(long id, [FromBody] DizimoModel dizimo)
+        {
+            var dizimoEntidade = new DizimoModel().Get(id);
+            dizimoEntidade.Update(id);
+
+            return Ok("Atualizado");
+        }
+        [HttpDelete("api/v1/Dizimos/{id}")]
+        public IActionResult Delete(long id)
+        {
+            var dizimo = new DizimoModel().Get(id);
+            dizimo.Delete(id);
+            
+
+            return Ok("Deletado");
         }
     }
 }
